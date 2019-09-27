b4accde00542198e7f36dbbb0c431f3df63f43fb
claudezhang <claude9493@users.noreply.github.com>
2019-09-27 16:19:39 +0800

    Apple Daily...

---
+++ appledaily.py
@@ -0,0 +1,20 @@
+import scrapy, string
+
+class AppleDailySpider(scrapy.Spider):
+    name = "appledaily"
+    # allowed_domains = ["https://hk.appledaily.com", "https://s.nextmedia.com"]
+    start_urls = ["https://hk.appledaily.com/hit"]
+
+    def parse(self, response):
+        hits = response.xpath("//div[contains(@class, 'item')]/div[contains(@class, 'text')]/a/@href").extract()
+        for url in hits:
+            self.logger.info('Find: %s', url)
+            yield scrapy.Request(url, callback=self.parse_news)
+    
+    def parse_news(self, response):
+        title = response.xpath("/html/head/title/text()").extract_first()
+        self.logger.info('Parsing: %s\n Title: %s'%(response.url, title))
+        content = response.xpath("//div[contains(@class, 'ArticleContent_Inner')]/p/text()").extract() 
+        with open('news.txt', 'a', encoding='utf-8') as f:
+            f.write(title)
+            f.write(''.join(content))
